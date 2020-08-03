1508a3784a85e6db460a7b4f5649f8d4949400ab
migte <68705081+migte@users.noreply.github.com>
2020-08-03 10:01:33 -0400

    All devices send data

--- Server/routes/deviceList.js
+++ Server/routes/deviceList.js
@@ -28,12 +28,9 @@ router.post('/:deviceStatus', (req, res, next) => {
             let name = String(info.model);
             let nameVal = name.split("(")
             // supported color changing devices (should probably add support for temperature only bulbs when temperature slider is added)
-            if (nameVal[0] == "LB130" || nameVal[0] == "LB230") {
-                res.send(info);
-            } else {
+            if (nameVal[0] == "LB100" || nameVal[0] == "LB120" || nameVal[0] == "LB130" || nameVal[0] == "LB200" || nameVal[0] == "LB230" || nameVal[0] == "KL110" || nameVal[0] == "KL130" || nameVal[0] == "HS100" || nameVal[0] == "HS200" || nameVal[0] == "HS210") {
                 res.send(info);
             }
-
         })
     });
 });
